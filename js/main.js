ab7928f80131d2da38a0ce7c16d7c6d97dffc83f
Mantosh Sah <59989979+mantu1738@users.noreply.github.com>
2021-04-13 23:26:45 +0530

    javascript tried

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-/****Changing words** */
+/*************Change the about tabs***************** */
+
+
+(()=>{
+    var aboutinfo = document.querySelector('.about-info2');
+    var itemContainer = document.getElementsByClassName('about-info-item');
+    for (var i = 0 ; i < itemContainer.length; i++){
+    itemContainer[i].addEventListener('click', ( e)=>{
+        if(e.target.classList.contains('about-info-item') && 
+        !e.target.classList.contains('active')){
+            const target1 = e.target.getAttribute("data-target");
+            for (var i = 0 ; i < itemContainer.length; i++){
+            itemContainer[i].querySelector(".active").classList.remove("outer-shadow", "active");
+            e.target.classList.add("active", "outershadow");
+            }
+           
+        }
+    });
+    }
+})();
