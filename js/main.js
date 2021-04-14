b0078c17a613e360784151c4e714e9c2dea8516e
Mantosh Sah <59989979+mantu1738@users.noreply.github.com>
2021-04-14 15:24:20 +0530

    javascript for changing tab is completed

--- js/main.js
+++ js/main.js
@@ -2,19 +2,34 @@
 
 
 (()=>{
-    var aboutinfo = document.querySelector('.about-info2');
-    var itemContainer = document.getElementsByClassName('about-info-item');
-    for (var i = 0 ; i < itemContainer.length; i++){
-    itemContainer[i].addEventListener('click', ( e)=>{
-        if(e.target.classList.contains('about-info-item') && 
-        !e.target.classList.contains('active')){
-            const target1 = e.target.getAttribute("data-target");
-            for (var i = 0 ; i < itemContainer.length; i++){
-            itemContainer[i].querySelector(".active").classList.remove("outer-shadow", "active");
-            e.target.classList.add("active", "outershadow");
-            }
+    const aboutSection = document.querySelector('.about-info');
+    const itemContainer =  document.querySelector('.about-info2');
+
+    itemContainer.addEventListener("click", (e) =>{
+        /*Check target contaon about-info-item class and not contaoins 
+        'active class */
+
+        if(e.target.classList.contains("about-info-item") &&
+        !e.target.classList.contains("active")){
+
+            const targeted = e.target.getAttribute("data-target");
            
+            // diable existing active about-info-item
+
+            itemContainer.querySelector(".active").classList.remove("outer-shadow", "active");
+
+            // activate new tab
+
+            e.target.classList.add("active", "outer-shadow");
+
+            //deactivate existing active about-info
+
+            document.querySelector(".about-content.active").classList.remove("active");
+
+            /// display other contents
+
+            document.querySelector(targeted).classList.add("active");
         }
+        
     });
-    }
 })();
